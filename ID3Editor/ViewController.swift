16718817f272436610aeda468b3769c897e5dfe9
Kavin <kavin.inbox@outlook.com>
2020-05-17 18:31:23 +0530

    Project Initial Commit

---
+++ ID3Editor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ID3Editor
+//
+//  Created by Kavin K on 17/05/20.
+//  Copyright © 2020 Kavin Softcorp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
